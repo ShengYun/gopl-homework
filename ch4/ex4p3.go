b99e7a42fc8e0c2cd3e0102e8bc2b26345b59637
Yun Sheng <uewing@gmail.com>
2018-05-01 23:24:46 -0400

    ex4p3: pointer to array inplace reverse

---
+++ ch4/ex4p3.go
@@ -0,0 +1,15 @@
+// Rewrite reverse to use an array pointer instead of a slice
+package ch4
+
+// original reverse function
+func reverseOrig(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func reverse(s *[3]int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
---
+++ ch4/ex4p3_test.go
@@ -0,0 +1,59 @@
+package ch4
+
+import (
+	"testing"
+)
+
+var tableTest = []struct {
+	input  []int
+	expect []int
+}{
+	{[]int{}, []int{}},
+	{[]int{1}, []int{1}},
+	{[]int{1, 2}, []int{2, 1}},
+	{[]int{1, 2, 3}, []int{3, 2, 1}},
+}
+
+var tableTest2 = []struct {
+	input  [3]int
+	expect [3]int
+}{
+	{[...]int{1, 2, 3}, [...]int{3, 2, 1}},
+}
+
+func sliceValueEqual(a []int, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestReverseOrig(t *testing.T) {
+
+	for _, v := range tableTest {
+		actual := v.input
+		reverseOrig(actual)
+		t.Logf("%v %v", actual, v.input)
+		if !sliceValueEqual(actual, v.expect) {
+			t.Errorf("reverseOrig(%s), expected %v, actual %v", v.input, v.expect, actual)
+		}
+	}
+}
+
+func TestReverse(t *testing.T) {
+	for _, v := range tableTest2 {
+		actual := v.input
+		reverse(&actual)
+		if !sliceValueEqual(actual[:], v.expect[:]) {
+			t.Errorf("reverseOrig(%s), expected %v, actual %v", v.input, v.expect, actual)
+		}
+	}
+
+}
